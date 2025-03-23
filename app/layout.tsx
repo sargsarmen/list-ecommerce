c575b6dedd30b8746e69720694057bedff0beb20
Armen Sargsyan <asargsyan@aramaze.com>
2025-03-23 19:00:12 +0400

    delete all unused components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from "react"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
 import { Navbar } from "@/components/navbar"
 import { Toaster } from "@/components/ui/toaster"
 import { ListingsProvider } from "@/context/listings-context"
@@ -17,17 +16,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
-          <ListingsProvider>
-            <TransactionsProvider>
-              <div className="flex flex-col min-h-screen">
-                <Navbar />
-                <main className="flex-1">{children}</main>
-              </div>
-              <Toaster />
-            </TransactionsProvider>
-          </ListingsProvider>
-        </ThemeProvider>
+        <ListingsProvider>
+          <TransactionsProvider>
+            <div className="flex flex-col min-h-screen">
+              <Navbar />
+              <main className="flex-1">{children}</main>
+            </div>
+            <Toaster />
+          </TransactionsProvider>
+        </ListingsProvider>
       </body>
     </html>
   )
